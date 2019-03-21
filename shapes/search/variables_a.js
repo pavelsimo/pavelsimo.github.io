5bdbbf41475fcd0afb5073098743cd10477a97cc
travis-ci <pavel.simo@gmail.com>
2019-03-21 23:45:00 +0000

    Update

--- shapes/search/variables_a.js
+++ shapes/search/variables_a.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['parser_5ferror',['parser_error',['../structsimo_1_1shapes_1_1_wkt_result.html#a673ccea2a0f276b7039f1f1d3c11f443',1,'simo::shapes::WktResult']]],
+  ['point',['point',['../unionsimo_1_1shapes_1_1_geometry.html#a40c1c897148ad460f5c35fd3d0abce76',1,'simo::shapes::Geometry']]],
   ['polygon',['polygon',['../unionsimo_1_1shapes_1_1_geometry.html#a91c03a104ba05c375c2435e36672a8b6',1,'simo::shapes::Geometry']]],
   ['precision',['precision',['../classsimo_1_1shapes_1_1_base_geometry.html#acae0a4cb31a3aad895298f84410bae7f',1,'simo::shapes::BaseGeometry']]]
 ];
