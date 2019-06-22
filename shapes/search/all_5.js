9bf6856ec43d626c06104e3e139ef13b8cb82af9
travis-ci <pavel.simo@gmail.com>
2019-06-22 16:26:38 +0000

    Update

--- shapes/search/all_5.js
+++ shapes/search/all_5.js
@@ -4,5 +4,6 @@ var searchData=
   ['geometry_5ferror',['geometry_error',['../classsimo_1_1shapes_1_1exceptions_1_1geometry__error.html#a0d6e2e21de870c774542273f07dfa0de',1,'simo::shapes::exceptions::geometry_error']]],
   ['geometry_5ferror',['geometry_error',['../classsimo_1_1shapes_1_1exceptions_1_1geometry__error.html',1,'simo::shapes::exceptions']]],
   ['geometry_5ft',['geometry_t',['../classsimo_1_1shapes_1_1geometry__t.html',1,'simo::shapes']]],
+  ['get_5fposition',['get_position',['../classsimo_1_1shapes_1_1wkt__lexer.html#a9ad0a03da77e1613f127172bf19f6128',1,'simo::shapes::wkt_lexer']]],
   ['get_5ftoken',['get_token',['../classsimo_1_1shapes_1_1wkt__lexer.html#a32267afbdbcd9110f561e99d9dd6dbda',1,'simo::shapes::wkt_lexer']]]
 ];
--- shapes/search/functions_5.js
+++ shapes/search/functions_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['geom_5ftype',['geom_type',['../classsimo_1_1shapes_1_1basic__geometry.html#a00038bcffeba8f69ad2cbf09bdba3d0b',1,'simo::shapes::basic_geometry']]],
   ['geometry_5ferror',['geometry_error',['../classsimo_1_1shapes_1_1exceptions_1_1geometry__error.html#a0d6e2e21de870c774542273f07dfa0de',1,'simo::shapes::exceptions::geometry_error']]],
+  ['get_5fposition',['get_position',['../classsimo_1_1shapes_1_1wkt__lexer.html#a9ad0a03da77e1613f127172bf19f6128',1,'simo::shapes::wkt_lexer']]],
   ['get_5ftoken',['get_token',['../classsimo_1_1shapes_1_1wkt__lexer.html#a32267afbdbcd9110f561e99d9dd6dbda',1,'simo::shapes::wkt_lexer']]]
 ];
